a0cc803a50bb52b9c569d39a17a60b6f80062785
Lin Huang <ceciliahuang1012@gmail.com>
2021-09-14 13:03:57 -0400

    Initial Commit

---
+++ Trail For Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trail For Git
+//
+//  Created by Cecilia Huang on 9/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
